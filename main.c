32b07545c135a206c3712c80e6ff87e77b6bfe35
Ioan <vivoicu12@gmail.com>
2016-11-01 13:08:46 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,53 @@
+#include"prototypes.h"
+FILE *file;
+
+
+int main()
+
+{
+    int i,n,x;
+    file=fopen("in.txt","r");
+    rt=NULL;
+    do{
+    printf("\n1.Adaugare noduri fisier");
+    printf("\n2.Sterge un nod");
+    printf("\n3.Preorder");
+    printf("\n4.Inorder");
+    printf("\n5.Postorder");
+	printf("\n6.Arborescenta");
+    printf("\n\nOptiunea dumneavoastra: ");
+    scanf("%d",&i);
+   switch(i)
+   {
+   case 1:
+    fscanf(file,"%d",&n);
+    do{
+        rt=Add_Node(rt,n);
+        fscanf(file,"%d",&n);
+    }while(!feof(file));
+    fclose(file);
+    break;
+   case 2:
+    printf("\nAlegeti nodul pe care doriti sa-l stergeti");
+    scanf("%d",&x);
+    rt=Delete(rt,x);
+    break;
+   case 3:
+    preorder(rt);
+    break;
+   case 4:
+    inorder(rt);
+    break;
+   case 5:
+    postorder(rt);
+    break;
+   case 6:
+	   Display(rt, 0);
+	   break;
+   }
+
+
+
+}while(i);
+return 0;
+}
---
+++ prototypes.h
@@ -0,0 +1,27 @@
+
+#ifndef PROTOTYPES_H_INCLUDED
+#define PROTOTYPES_H_INCLUDED
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct _Node
+{
+    int data;
+    struct _Node *left;
+    struct _Node *right;
+}
+Node;
+
+Node  *rt;
+void Display(Node* root, int nivel);
+Node *Add_Node(Node *root,int data);
+void preorder (Node *root);
+void inorder(Node *root);
+void postorder(Node *root);
+Node* FindMin(Node* root);
+Node* Delete( Node *root, int data);
+
+
+
+#endif // PROTOTYPES_H_INCLUDED
